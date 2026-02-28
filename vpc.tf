19988ba7ae643f563b6cc09cd7dc717d1ebc5f8e
SATYAVARDHAN04 <svardhan278@gmail.com>
2026-02-28 13:30:18 +0530

    terraform

--- vpc.tf
+++ vpc.tf
@@ -18,34 +18,41 @@ resource "aws_internet_gateway" "roboshop-igw" {
   })
 }
 
-#roboshop-dev-subnet-us-east-1a
+#roboshop-dev-public-us-east-1a
 resource "aws_subnet" "roboshop-public" {
   vpc_id     = aws_vpc.roboshop.id
   count = length(var.public_subnet_cidr)
   cidr_block = var.public_subnet_cidr[count.index]
   availability_zone = data.aws_availability_zones.availableaz.names[count.index]
+  map_public_ip_on_launch = true
 
   tags = merge(local.common_tags,{
     Name="${var.project}-${var.environment}-public-${data.aws_availability_zones.availableaz.names[count.index]}"
   })
 }
 
-# resource "aws_subnet" "main" {
-#   vpc_id     = aws_vpc.roboshop.id
-#   count = length(var.public_subnet_cidr)
-#   cidr_block = var.public_subnet_cidr[count.index]
+#roboshop-dev-private-us-east-1a
+resource "aws_subnet" "roboshop-private" {
+  vpc_id     = aws_vpc.roboshop.id
+  count = length(var.private_subnet_cidr)
+  cidr_block = var.private_subnet_cidr[count.index]
+  availability_zone = data.aws_availability_zones.availableaz.names[count.index]
+  map_public_ip_on_launch = true
 
-#   tags = {
-#     Name = "Main"
-#   }
-# }
+  tags = merge(local.common_tags,{
+    Name="${var.project}-${var.environment}-private-${data.aws_availability_zones.availableaz.names[count.index]}"
+  })
+}
 
-# resource "aws_subnet" "main" {
-#   vpc_id     = aws_vpc.roboshop.id
-#   count = length(var.public_subnet_cidr)
-#   cidr_block = var.public_subnet_cidr[count.index]
+#roboshop-dev-database-us-east-1a
+resource "aws_subnet" "roboshop-databse" {
+  vpc_id     = aws_vpc.roboshop.id
+  count = length(var.database_subnet_cidr)
+  cidr_block = var.database_subnet_cidr[count.index]
+  availability_zone = data.aws_availability_zones.availableaz.names[count.index]
+  map_public_ip_on_launch = true
 
-#   tags = {
-#     Name = "Main"
-#   }
-# }
+  tags = merge(local.common_tags,{
+    Name="${var.project}-${var.environment}-databse-${data.aws_availability_zones.availableaz.names[count.index]}"
+  })
+}
