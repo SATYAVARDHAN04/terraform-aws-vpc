b0e16bb1dc31ac4e433799cb7be1a56002b80dd5
SATYAVARDHAN04 <svardhan278@gmail.com>
2026-02-28 12:59:22 +0530

    terraform

--- locals.tf
+++ locals.tf
@@ -2,6 +2,5 @@ locals {
   common_tags={
     project=var.project
     environment=var.environment
-    Name="${var.project}-${var.environment}-vpc"
   }
 }
--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,19 @@
 # roboshop-dev-vpc
-
+# roboshop-dev-igw
 resource "aws_vpc" "roboshop" {
   cidr_block       = var.cidr
   instance_tenancy = "default"
   enable_dns_hostnames = "true"
 
-  tags = local.common_tags
+  tags = merge(local.common_tags,{
+    Name="${var.project}-${var.environment}-vpc"
+  })
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.roboshop.id
+
+  tags = merge(local.common_tags,{
+    Name="${var.project}-${var.environment}-igw"
+  })
 }
