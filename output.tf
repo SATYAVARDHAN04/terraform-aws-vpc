6f8e74603c05fff8e195aca0e40a6ad718bbae2f
SATYAVARDHAN04 <svardhan278@gmail.com>
2026-03-05 16:22:36 +0530

    terraform

--- output.tf
+++ output.tf
@@ -5,3 +5,11 @@ output "vpc_id" {
 output "public_subnet_id" {
   value = aws_subnet.roboshop-public[*].id
 }
+
+output "private_subnet_id" {
+  value = aws_subnet.roboshop-private[*].id
+}
+
+output "database_subnet_id" {
+  value = aws_subnet.roboshop-database[*].id
+}
